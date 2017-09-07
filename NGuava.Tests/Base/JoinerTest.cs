4db6dce2abab49f2660957c62690b5b6dc3353c9
lesiak <lesiak@outlook.com>
2017-09-07 09:01:57 +0200

    Joiner.UseForNull

--- NGuava.Tests/Base/JoinerTest.cs
+++ NGuava.Tests/Base/JoinerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NGuava.Base;
 
@@ -6,12 +7,67 @@ namespace NGuava.Tests.Base
     [TestClass]
     public class JoinerTest
     {
+
         [TestMethod]
-        public void TestTwoStingArguments()
+        public void TestOneString()
         {
-            var testInput = new[] {"a", "b"};
+            var testInput = new[] { "a" };
+            var result = Joiner.On(", ").Join(testInput);
+            Assert.AreEqual("a", result);
+        }
+
+
+        [TestMethod]
+        public void TestOneInt()
+        {
+            var testInput = new[] { 1 };
+            var result = Joiner.On(", ").Join(testInput);
+            Assert.AreEqual("1", result);
+        }
+
+
+        [TestMethod]
+        public void TestTwoInts()
+        {
+            var testInput = new[] {1, 2};
+            var result = Joiner.On(", ").Join(testInput);
+            Assert.AreEqual("1, 2", result);
+        }
+
+
+        [TestMethod]
+        public void TestTwoStrings()
+        {
+            var testInput = new[] { "a", "b" };
             var result = Joiner.On(", ").Join(testInput);
             Assert.AreEqual("a, b", result);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestStringAndNull()
+        {
+            var testInput = new[] {"a", null};
+            Joiner.On(", ").Join(testInput);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestNullAndString()
+        {
+            var testInput = new[] { null, "b" };
+            Joiner.On(", ").Join(testInput);
+        }
+
+
+        [TestMethod]
+        public void TestStringAndNullSubstituted()
+        {
+            var testInput = new[] {"a", null};
+            var result = Joiner.On(", ").UseForNull("null").Join(testInput);
+            Assert.AreEqual("a, null", result);
         }
     }
 }
--- NGuava/Base/Joiner.cs
+++ NGuava/Base/Joiner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace NGuava.Base
@@ -12,9 +13,12 @@ namespace NGuava.Base
             return new Joiner(separator);
         }
 
-         private Joiner(string separator) {
+
+        private Joiner(string separator)
+        {
             this.separator = Preconditions.CheckNotNull(separator);
-         }
+        }
+
 
         public StringBuilder AppendTo<T>(StringBuilder stringBuilder, IEnumerator<T> parts)
         {
@@ -32,21 +36,54 @@ namespace NGuava.Base
             return stringBuilder;
         }
 
-        string ToString(object part)
+
+        protected virtual string ToString(object part)
         {
             Preconditions.CheckNotNull(part);
-            return (part is string) ? (string)part : part.ToString();
+            return (part is string s) ? s : part.ToString();
         }
+
 
         public string Join<T>(IEnumerable<T> parts)
         {
             return Join(parts.GetEnumerator());
         }
 
+
         public string Join<T>(IEnumerator<T> parts)
         {
             return AppendTo(new StringBuilder(), parts).ToString();
         }
 
+
+        public virtual Joiner UseForNull(string nullText)
+        {
+            Preconditions.CheckNotNull(nullText);
+            return new UseForNullJoiner(this, nullText);
+        }
+
+
+        private class UseForNullJoiner : Joiner
+        {
+            private readonly string nullText;
+
+
+            public UseForNullJoiner(Joiner prototype, string nullText) : base(prototype.separator)
+            {
+                this.nullText = nullText;
+            }
+
+
+            protected override string ToString(object part)
+            {
+                return(part == null) ? nullText : base.ToString(part);
+            }
+
+
+            public override Joiner UseForNull(string newNullText)
+            {
+                throw new InvalidOperationException("already specified UseForNull");
+            }
+        }
     }
 }
